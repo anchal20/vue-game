1fe084e80c8128e47fb0aed96c25cddbe7f0d84c
Anchal <anchalsachdeva20@gmail.com>
2017-02-11 20:03:24 +0530

    final game

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,18 @@
 		<input v-model.number="columns">
 		<br>
 		<br>
-		<puzzle :rows="rows" :columns="columns" :colorArray="getColorArray"></puzzle>
+		<div class="wrapper">
+			<div class="rows" >
+				<div class="columns" v-for="(block, index) in blocks"
+				@click.prevent="activeColors.includes(backgroundColors[index]) || selectedIndx.includes(index) ? 'null' : flipCard(index)"
+				:class="{
+					active: activeColors.includes(backgroundColors[index]),
+					selected: selectedIndx.includes(index)
+					}">
+					<puzzle :color="backgroundColors[index]" ></puzzle>
+				</div>
+			</div>
+		</div>
   </div>
 </template>
 
@@ -23,10 +34,35 @@ export default {
 	data: function() {
 		return {
 			rows: 4,
-			columns: 3
+			columns: 3,
+			backgroundColors: [],
+			activeColors: [],
+			selectedIndx: []
 		}
 	},
 	computed: {
+		blocks: function() {
+			return (this.rows * this.columns);
+		}
+	},
+	created: function() {
+		this.setBackgroundColors();
+	},
+	watch: {
+		rows: function() {
+			this.reset();
+			this.resetRowColumn();
+		},
+		columns: function() {
+			this.reset();
+			this.resetRowColumn();
+		}
+	},
+	methods: {
+		setBackgroundColors: function() {
+			var colorArray = this.getColorArray();
+			this.$set(this, 'backgroundColors', colorArray);
+		},
 		getColorArray: function() {
 			let blocks = (this.rows * this.columns)/2;
 			let colorArray = [];
@@ -35,18 +71,56 @@ export default {
 				let blue = Math.round(Math.random() * 256);
 				let green = Math.round(Math.random() * 256);
 				let color = "rgb(" + red + "," + blue + "," + green + ")";
-				colorArray.push(color);
-				colorArray.push(color);
+				colorArray.push(...[color, color]);
+				// colorArray.push(color);
 			}
-			console.log(colorArray);
 			//this.$set(this, colorArray, colorArray);
+			var counter = colorArray.length, temp, index;
+	   	// While there are elements in the array
+	   	while (counter > 0) {
+        	// Pick a random index
+        	index = Math.floor(Math.random() * counter);
+        	// Decrease counter by 1
+        	counter--;
+        	// And swap the last element with it
+        	temp = colorArray[counter];
+        	colorArray[counter] = colorArray[index];
+        	colorArray[index] = temp;
+	    	}
 			return colorArray;
-		}
-	},
-	created: function() {
+		},
 
-	},
-	methods: {
+		setColorArray: function(array) {
+			this.$set(this, colorArray, array);
+		},
+		flipCard: function(index) {
+			if(!this.selectedIndx.length) {
+				this.selectedIndx.push(index);
+			} else {
+				if(this.backgroundColors[index] == this.backgroundColors[this.selectedIndx[0]]) {
+					this.activeColors.push(this.backgroundColors[index]);
+					this.selectedIndx = [];
+				} else {
+					this.selectedIndx.push(index);
+					setTimeout(() => {
+						this.selectedIndx = [];
+					}, 1000);
+				}
+			}
+		},
+		reset: function() {
+			this.setBackgroundColors();
+			this.activeColors = [],
+			this.selectedIndx = []
+		},
+		resetRowColumn: function() {
+			if(this.blocks%2 !== 0) {
+				alert('Only even blocks are allowed, Your puzzle is reset to original value');
+				this.reset();
+				this.rows = 4;
+				this.columns = 3;
+			}
+		}
 
 	}
 }
@@ -60,5 +134,49 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.wrapper {
+	max-width: 600px;
+	margin: 0 auto;
+}
+.rows {
+	display: flex;
+	flex-wrap: wrap;
+	flex: 1;
+	justify-content: center;
+}
+.columns {
+	border: 1px solid #000;
+	perspective: 1000px;
+	display: flex;
+	cursor: pointer;
+	width: 25%;
+	height: 50px;
+}
+.flipper {
+	transition: 0.6s;
+	transform-style: preserve-3d;
+	position: relative;
+}
+.flipper, .front, .back {
+	width: 100%;
+	height: 100%;
+}
+.front, .back {
+	backface-visibility: hidden;
+	position: absolute;
+	top: 0;
+	left: 0;
+}
+.front {
+	z-index: 2;
+	transform: rotateY(0deg);
+	background-color: pink;
+}
+.back {
+	transform: rotateY(180deg);
+}
+.columns.active .flipper, .columns.selected .flipper {
+	transform: rotateY(180deg);
 }
 </style>
--- src/components/Puzzle.vue
+++ src/components/Puzzle.vue
@@ -1,28 +1,22 @@
 <template lang="html">
-	<div class="">
-		<div class="frame">
-			<div class="rows" v-for="(row, rIndex) in rows">
-				<div class="columns" v-for="(column, index) in columns" @click='selectBox($event)' :data-row="rIndex">
 					<div class="flipper">
 						<div class="front">
 						</div>
-						<div class="back" v-bind:style="[getRandomColor()]">
-
+						<div class="back" v-bind:style="'background-color:' + color">
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</div>
 </template>
 
 <script>
-
+var temp = [0];
 export default {
-	props: ['rows', 'columns', 'colorArray'],
+	props: ['color'],
 	data: function() {
 		return {
-			counter: 0
+			succesColors: [],
+			selectedColor: [],
+			currentColor: null
 		}
 	},
 	computed: {
@@ -37,65 +31,40 @@ export default {
 
 	},
 	created: function() {
-		
+
 	},
 	methods: {
-		selectBox: function(e) {
-			console.log(e.target.parentElement.parentElement);
-			e.target.parentElement.parentElement.classList.toggle('hover');
+		selectBox: function(color) {
+			// e.target.parentElement.parentElement.classList.toggle('hover');
+			console.log(color['background-color']);
+			var col = color['background-color'];
+			this.selectedColor.push(col);
+			console.log(this.selectedColor.length);
+			if(this.selectedColor.length > 1) {
+				if(this.selectedColor[0] == this.selectedColor[1]) {
+					this.succesColors.push(this.selectedColor[0]);
+					this.selectedColor = [];
+				} else {
+					this.selectedColor = [];
+				}
+			}
 		},
 		getRandomColor: function() {
-			var random = Math.floor(Math.random() * this.colorCount);
+			var random = 0;
+			var colorArray = this.colorArray;
+			var color = this.colorArray[random];
+			colorArray.splice(random, 1);
+			this.$set(this, color, color);
+			this.$emit('setColorArray', colorArray);
 			return {
-				'background-color': this.colorArray[random]
+				'background-color': color
 			}
+
 		}
 	}
 }
 </script>
 
 <style lang="css">
-	.frame {
-		margin: 0 auto;
-		border: 1px solid #000;
-		max-width: 920px;
-	}
-	.rows {
-		height: 100px;
-		border-bottom: 1px solid #000;
-		display: flex;
-		flex-direction: row;
-	}
-	.columns {
-		border: 1px solid #000;
-		perspective: 1000px;
-		flex-grow: 1;
-		cursor: pointer;
-	}
-	.flipper {
-		transition: 0.6s;
-		transform-style: preserve-3d;
-		position: relative;
-	}
-	.flipper, .front, .back {
-		width: 100%;
-		height: 100%;
-	}
-	.front, .back {
-		backface-visibility: hidden;
-		position: absolute;
-		top: 0;
-		left: 0;
-	}
-	.front {
-		z-index: 2;
-		transform: rotateY(0deg);
-		background-color: pink;
-	}
-	.back {
-		transform: rotateY(180deg);
-	}
-	.columns.hover .flipper {
-		transform: rotateY(180deg);
-	}
+
 </style>
